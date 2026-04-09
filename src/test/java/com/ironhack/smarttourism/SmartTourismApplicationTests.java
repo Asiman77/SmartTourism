fd9421ad49c5bb13e321db392e183bd708e363ec
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-04-09 17:34:48 +0400

    Initial commit

---
+++ src/test/java/com/ironhack/smarttourism/SmartTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.smarttourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTourismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
