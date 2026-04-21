7dc601ff541c80a9152c9d7520b1697ca8465d2e
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-04-21 15:17:37 +0400

    DataLoader added

---
+++ src/main/java/com/ironhack/smarttourism/config/DataLoader.java
@@ -0,0 +1,39 @@
+package com.ironhack.smarttourism.config;
+
+import com.ironhack.smarttourism.entity.User;
+import com.ironhack.smarttourism.entity.enums.RoleName;
+import com.ironhack.smarttourism.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+
+        String adminEmail = "admin@smarttourism.com";
+
+        if (userRepository.existsByEmail(adminEmail)) {
+            return; // artıq varsa heç nə etmir
+        }
+
+        User admin = new User();
+        admin.setFullName("System Admin");
+        admin.setEmail(adminEmail);
+        admin.setPassword(passwordEncoder.encode("admin123")); // şifrə
+        admin.setPhone("0500000000");
+        admin.setActive(true);
+        admin.setRole(RoleName.ADMIN);
+
+        userRepository.save(admin);
+
+        System.out.println("✅ Admin user created: admin@smarttourism.com / admin123");
+    }
+}
